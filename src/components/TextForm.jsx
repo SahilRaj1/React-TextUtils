bfede4e9e09e8c8a264a1496baeadc396e721825
SahilRaj1 <raj24sahil@gmail.com>
2022-07-09 22:59:38 +0530

    final

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -17,10 +17,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        let text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Text Copied", "success");
     }
 
@@ -59,7 +56,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{color: props.mode === "light"? "#212529":"white"}}>
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
                 <h2>Preview</h2>
                 <p>{text.length>0? text : "Enter something above to preview"}</p>
             </div>
